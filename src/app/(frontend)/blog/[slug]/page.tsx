51d178c9d90c27bb1ef019688c82c7a1b2140594
Samantha <samantha@uppointinc.com>
2025-05-16 13:59:44 -0700

    trying to fix promise erorr

--- src/app/(frontend)/blog/[slug]/page.tsx
+++ src/app/(frontend)/blog/[slug]/page.tsx
@@ -1,16 +1,13 @@
 import { getPayload } from "payload";
 import config from '@payload-config';
 
-type PostPageProps = {
-    params: {
-        slug: string;
-    }
-}
 // individual post page
-export default async function PostPage(props: PostPageProps) {
-    const { params } = props;
-    // const { slug } = params;
-    const payload = await getPayload({config});
+export default async function PostPage({params,
+}: {
+    params: { slug:string}
+}) {
+   
+const payload = await getPayload({config});
 const {docs} = await payload.find({
     collection: 'posts', //required
     depth: 2,
@@ -20,7 +17,7 @@ const {docs} = await payload.find({
         }
     }
 })
-const posts = docs[0];
+const posts = docs?.[0];
 console.log('Post:', posts); // Debugging
  return (
     <div className="p-6">
