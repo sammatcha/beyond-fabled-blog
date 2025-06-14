04908f9e1fbdbc4eb0958278fc8d737e29ffc766
Samantha <samantha@uppointinc.com>
2025-06-14 00:05:04 -0700

    fixed mobile nav

--- src/app/(frontend)/components/Hero.tsx
+++ src/app/(frontend)/components/Hero.tsx
@@ -5,16 +5,15 @@ export default function Hero(){
      return(
       //outer container - centers rectangle
         <div className="flex items-center justify-center min-w-full h-screen  overflow-y-hidden "> 
-        <div className='relative w-[80vw] max-w-[2200px] 
-        h-[70vh] 
+        <div className='relative w-[80vw] max-w-[2200px] min-w-2xs
+        h-[40vh] sm:h-[50vh] md:h-[60vh] lg:h-[70vh] 
         bg-white rounded-lg shadow-lg  overflow-y-hidden flex items-center justify-center      '>
            
       
       <Image  
       src = {"/image/bubblex4.png"}
       alt="background image"
-      width={1800}
-      height={1800}
+      fill
       className={"object-cover w-full h-full"}
       priority
       />
@@ -30,3 +29,4 @@ export default function Hero(){
 // h-[30vh] md:h-[70vh] sm:min-h-[100px]
 
 // h-[calc(100vh-100px)
+
--- src/app/(frontend)/components/nav/MobileNav.tsx
+++ src/app/(frontend)/components/nav/MobileNav.tsx
@@ -29,12 +29,12 @@ export default function MobileNav(){
         <nav className="block lg:hidden relative z-20 ">
            
                             {/* x button */}
-                        <button onClick={handleOnClick} className="absolute z-50 mt-3 left-0 px-2 cursor-pointer">
+                        <button onClick={handleOnClick} className="absolute z-50 mt-10 left-0 px-5  cursor-pointer">
                             {isOpen ? (
-                                <CircleX className=" w-8 text-black "  />  
+                                <CircleX className="w-8 text-black "  />  
                             ) : (
                                     
-                                < Menu className="w-10 text-black" />
+                                < Menu className="w-15 text-black" />
                             )
                         }
                               
@@ -43,12 +43,12 @@ export default function MobileNav(){
                         {isOpen && (
                             <div className="fixed h-full w-full float-right bg-black/20 z-0">  
                             <div className="fixed w-[30vh] max-w-xs bg-[#FDFBF7] z-40 h-screen  space-x-6 pt-10  px-5 shadow-2xl shadow-orange-100">
-                                <div className="pt-6 mb-5">
+                                <div className="pt-10">
                                      <Link href={"/"} className="font-caveatBrush text-cuteBlue text-xl border-b hover:text-[#59775C]">Home</Link>
                                 </div>
                               
                         {filteredLinks.map((link,id) => (
-                        <Link key={id} href={link.link} className="flex flex-col  ml-1 mt-10 text-cuteBlue font-karla font-bold hover:text-[#93CED6]" > 
+                        <Link key={id} href={link.link} className="flex flex-col  mt-10 text-cuteBlue font-karla font-bold hover:text-[#93CED6]" > 
                         {link.name}
                         </Link>
                   
