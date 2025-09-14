ee81f7ef6aa9d7abe1bd6852ee4b6a1c4e9222d2
Samantha <samantha@uppointinc.com>
2025-09-14 15:02:02 -0700

    pushing without unoptimized again to test

--- src/app/(frontend)/components/Hero.tsx
+++ src/app/(frontend)/components/Hero.tsx
@@ -8,7 +8,7 @@ export default function Hero(){
                     {/* wrapper with wash + text */}
                   <div className=' '>
                     <div className='relative w-[55vw] min-w-[350px] max-w-[600px]'>
-                        <Image src="/image/hero-bg-wash.png" alt="hero wash" width={600} height={600} unoptimized className="opacity-80 object-cover h-auto "/>
+                        <Image src="/image/hero-bg-wash.png" alt="hero wash" width={600} height={600}  className="opacity-80 object-cover h-auto "/>
                           <h1 className='absolute inset-0 flex flex-col justify-center items-center font-playfairDisplay text-5xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl tracking-wide w-full'>
                            Welcome to
                           <span className='font-styleScript font-bold text-darkSageGreen relative bottom-1 left-0.5 '>Beyond Fabled</span>
@@ -20,8 +20,7 @@ export default function Hero(){
               
               <div className=' max-w-full  '>
                 <div className='relative  w-[30vw] min-w-[180px] max-w-[400px] '>
-                    <Image src="/image/hero-bg-bubble1.png" alt="hero bubbles" width={400} height={400} unoptimized className="object-contain lg:object-cover h-auto "/>
-
+                    <Image src="/image/hero-bg-bubble1.png" alt="hero bubbles" width={400} height={400}  className="object-contain lg:object-cover h-auto "/>
                 </div>
               </div>
            
